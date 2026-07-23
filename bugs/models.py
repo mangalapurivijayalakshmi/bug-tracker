51e8f45ac7752044e0c994304830f6674bbf373c
Mangalapuri Vijaya Lakshmi <mangalapurivijayalakshmi1432@gmail.com>
2026-07-23 16:19:06 +0530

    Update models.py

--- bugs/models.py
+++ bugs/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
 # Bug Model (this creates a "bugs" table in database)
@@ -25,8 +22,18 @@ class Bug(models.Model):
     description = models.TextField()                       # Bug details
     priority    = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='Medium')
     status      = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Open')
-    created_by  = models.ForeignKey(User, on_delete=models.CASCADE)  # Who created it
+    created_by  = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,related_name='reported_bugs') 
+    assigned_to  = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='assigned_bugs'
+    )  # Who is fixing the bug
     created_at  = models.DateTimeField(auto_now_add=True)  # Auto date/time
+    updated_at   = models.DateTimeField(auto_now=True)        # Auto updates every save
 
+    class Meta:
+        ordering = ['-created_at']   # Latest bugs show first
     def __str__(self):
         return self.title
